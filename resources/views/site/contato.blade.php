06990b645e4516ae0068cff967174c857eb9eb71
Jordana Loreto <jordanaloreto45@gmail.com>
2025-05-19 21:02:23 -0300

    estudos

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,12 @@
+<h3>contato(view)</h3>
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', [\App\Http\Controllers\ContatoController::class, 'login'])->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', [\App\Http\Controllers\ContatoController::class, 'clientes'])->name('app.clientes');
+    Route::get('/fornecedores', [\App\Http\Controllers\ContatoController::class, 'fornecedores'])->name('app.fornecedores');
+    Route::get('/produtos', [\App\Http\Controllers\ContatoController::class, 'produtos'])->name('app.produtos');
+});
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+return redirect()->route('site.rota1');//FUNCIONA TIPO O REDIRECT NO CONTROLADOR
+})->name('site.rota2');
+
+// Route::redirect(' /rota2', '/rota1');//REDIRECT PELA ROTA
+Route::fallback(function(){
+    echo 'A rota acessada nao existe. <a href="'.route('site.index').'">clique aqui</a>';
+});
